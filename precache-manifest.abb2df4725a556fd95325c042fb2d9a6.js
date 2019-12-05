4445df56fac7bb6bde44651cadf0f210860ebfe7
Juan David Castro <me@juandc.co>
2019-12-04 21:15:42 -0500

    Deplot GH Pages

---
+++ precache-manifest.abb2df4725a556fd95325c042fb2d9a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89458a246fd0383443db42849709acd0",
+    "url": "/usestate-vs-usereducer/index.html"
+  },
+  {
+    "revision": "8cbbd195c8776c3a020c",
+    "url": "/usestate-vs-usereducer/static/css/main.23d5776f.chunk.css"
+  },
+  {
+    "revision": "3decc5ec32f021a8e2bc",
+    "url": "/usestate-vs-usereducer/static/js/2.f53cb198.chunk.js"
+  },
+  {
+    "revision": "8cbbd195c8776c3a020c",
+    "url": "/usestate-vs-usereducer/static/js/main.30fc8fd7.chunk.js"
+  },
+  {
+    "revision": "3e4267892c445dca16e7",
+    "url": "/usestate-vs-usereducer/static/js/runtime-main.102619ee.js"
+  }
+]);
